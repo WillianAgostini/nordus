5c7eb9b7e5c44382239ff1a6205c3acffde20e77
Willian Agostini <willian.agostini@gmail.com>
2023-08-09 14:47:49 -0300

    feat: shoud possible to throw error on interceptors

--- tests/interceptors.spec.ts
+++ tests/interceptors.spec.ts
@@ -1,6 +1,7 @@
-import { create, post } from "../src";
+import { create, get, post } from "../src";
 import { FetchMock } from "jest-fetch-mock";
 import { InterceptorRequest, InterceptorResponse } from "../src/request";
+import { delay } from "./helpers/utils";
 
 describe("interceptors", () => {
   const fetchMock = fetch as FetchMock;
@@ -384,5 +385,107 @@ describe("interceptors", () => {
 
     await instance.get("/todos/1");
     expect(textInterceptor).toEqual("");
+  });
+
+  it("shoud possible to change headers on async interceptors on create instance", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ test: "test" }));
+    const instance = create({
+      baseURL: "http://localhost:5000",
+      interceptors: {
+        request: async (err, request) => {
+          await delay(1);
+          request.headers.set("access_token", "Bearer 123");
+        },
+        response: async (err, response) => {
+          await delay(1);
+          return response;
+        },
+      },
+    });
+    await instance.get("/todos/1");
+    const lastCall = fetchMock?.mock?.lastCall?.at(0) as Request;
+    expect(lastCall?.headers.get("access_token")).toEqual("Bearer 123");
+  });
+
+  it("shoud possible to change headers on get request with interceptors", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ test: "test" }));
+    await get("http://localhost:5000/todos/1", {
+      interceptors: {
+        request: async (err, request) => {
+          await delay(1);
+          request.headers.set("access_token", "Bearer 123");
+        },
+        response: async (err, response) => {
+          await delay(1);
+          return response;
+        },
+      },
+    });
+    const lastCall = fetchMock?.mock?.lastCall?.at(0) as Request;
+    expect(lastCall?.headers.get("access_token")).toEqual("Bearer 123");
+  });
+
+  it("shoud possible to throw error on request", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ test: "test" }));
+
+    let textInterceptorRequest = "";
+    const firstInterceptorRequest: InterceptorRequest = () => {
+      textInterceptorRequest += "1";
+      throw new Error("Error on interceptor");
+    };
+    const secondInterceptorRequest: InterceptorRequest = () => {
+      textInterceptorRequest += "2";
+    };
+
+    let textInterceptorResponse = "";
+    const firstInterceptorResponse: InterceptorResponse = () => {
+      textInterceptorResponse += "1";
+    };
+    try {
+      await get("http://localhost:5000/todos/1", {
+        interceptors: {
+          request: [firstInterceptorRequest, secondInterceptorRequest],
+          response: [firstInterceptorResponse],
+        },
+      });
+    } catch (error) {
+      expect(error.message).toEqual("Error on interceptor");
+    }
+    expect(textInterceptorRequest).toEqual("1");
+    expect(textInterceptorResponse).toEqual("");
+  });
+
+  it("shoud possible to throw error on response", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ test: "test" }));
+
+    let textInterceptorRequest = "";
+    const firstInterceptorRequest: InterceptorRequest = () => {
+      textInterceptorRequest += "1";
+    };
+    const secondInterceptorRequest: InterceptorRequest = () => {
+      textInterceptorRequest += "2";
+    };
+
+    let textInterceptorResponse = "";
+    const firstInterceptorResponse: InterceptorResponse = () => {
+      textInterceptorResponse += "1";
+      throw new Error("Error on interceptor");
+    };
+    const secondInterceptorResponse: InterceptorResponse = () => {
+      textInterceptorResponse += "2";
+    };
+
+    try {
+      await get("http://localhost:5000/todos/1", {
+        interceptors: {
+          request: [firstInterceptorRequest, secondInterceptorRequest],
+          response: [firstInterceptorResponse, secondInterceptorResponse],
+        },
+      });
+    } catch (error) {
+      expect(error.message).toEqual("Error on interceptor");
+    }
+    expect(textInterceptorRequest).toEqual("12");
+    expect(textInterceptorResponse).toEqual("1");
   });
 });
